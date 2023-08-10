c0848a7dbe5a906317342419837c0a7161d916a4
Kishan Khatrani <38881285+kkpkishan@users.noreply.github.com>
2023-08-10 14:12:08 +0530

    Update neuvector-k8s.sh

--- neuvector-k8s.sh
+++ neuvector-k8s.sh
@@ -55,6 +55,8 @@ done
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
+sleep 15
+
 # Helm installation
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 2>&1
 chmod 700 get_helm.sh 2>&1
